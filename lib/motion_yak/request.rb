09e42d4a86b24e0f988ee284ceb14eb4a075359e
Scott Ballantyne <ussballantyne@gmail.com>
2012-10-05 14:05:10 +0800

    sweet, got it working

--- lib/motion_yak/request.rb
+++ lib/motion_yak/request.rb
@@ -1,27 +1,27 @@
 module MotionYak
   module Request
-   def self.get(url)
+   def self.get(url, &block)
      BW::HTTP.get(url) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       yield(json)
+       block.call(json)
      end
    end
    def self.put(url, data)
      BW::HTTP.put(url, {payload: data}) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       yield(json)
+       block.call(json)
      end
    end
    def self.post(url, data)
      BW::HTTP.post(url, {payload: data}) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       yield(json)
+       block.call(json)
      end
    end
    def self.delete(url)
      BW::HTTP.delete(url) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       yield(json)
+       block.call(json)
      end
    end
   end
