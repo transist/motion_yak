58d664e7a385eedacb8aa6400d3ae5abae30802c
Scott Ballantyne <ussballantyne@gmail.com>
2012-10-05 13:49:39 +0800

    trying to use a proc

--- lib/motion_yak/request.rb
+++ lib/motion_yak/request.rb
@@ -1,27 +1,27 @@
 module MotionYak
   module Request
-   def self.get(url)
+   def self.get(url, &proc)
      BW::HTTP.get(url) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       return json
+       proc.call(json)
      end
    end
-   def self.put(url, data)
+   def self.put(url, data, &proc)
      BW::HTTP.put(url, {payload: data}) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       return json
+       proc.call(json)
      end
    end
-   def self.post(url, data)
+   def self.post(url, data, &proc)
      BW::HTTP.post(url, {payload: data}) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       return json
+       proc.call(json)
      end
    end
-   def self.delete(url)
+   def self.delete(url, &proc)
      BW::HTTP.delete(url) do |response|
        json = BW::JSON.parse(response.body.to_str)
-       return json
+       proc.call(json)
      end
    end
   end
