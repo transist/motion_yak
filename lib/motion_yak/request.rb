9691ea5553702c5201e5e5caa759e9e76ed46b4a
Scott Ballantyne <ussballantyne@gmail.com>
2012-10-05 13:27:50 +0800

    oops

--- lib/motion_yak/request.rb
+++ lib/motion_yak/request.rb
@@ -2,32 +2,32 @@ module MotionYak
   module Request
    def self.get(url)
      BW::HTTP.get(url) do |response|
-       json = parse(response)
+       json = parse(response.body.to_str)
        return json
      end
    end
    def self.put(url, data)
      BW::HTTP.put(url, {payload: data}) do |response|
-       json = parse(response)
+       json = parse(response.body.to_str)
        return json
      end
    end
    def self.post(url, data)
      BW::HTTP.post(url, {payload: data}) do |response|
-       json = parse(response)
+       json = parse(response.body.to_str)
        return json
      end
    end
    def self.delete(url)
      BW::HTTP.delete(url) do |response|
-       json = parse(response)
+       json = parse(response.body.to_str)
        return json
      end
    end
    
    def self.parse(string)
     json = MotionYak::JSON.parse(string)
-    p json
+    p json.inspect
     json
    end
   end
